e6abd1c65d28f1f427e59bf206468d76fb57d539
tailwindfusion <tailwindfusion@gmail.com>
2024-06-25 16:26:22 +0530

    Improved code quality

--- src/stores/blogStore.ts
+++ src/stores/blogStore.ts
@@ -1,57 +1,31 @@
 // src/stores/blogStore.ts
 
 import { defineStore } from 'pinia';
+import axios from 'axios';
+import { ref } from 'vue';
 
-export interface Blog {
+interface Blog {
+  albumId: number;
   id: number;
   title: string;
-  image: string;
-  content: string; // Add content property if it exists in your blog data
+  url: string; // Add content property if it exists in your blog data
+  thumbnailUrl: string; // Add content property if it exists in your blog data
 }
 
-interface BlogState {
-  blogs: Blog[];
-  loading: boolean;
-  error: string | null;
-}
+export const useBlogStore = defineStore('blog', () => {
 
-export const useBlogStore = defineStore('blogStore', {
-  state: (): BlogState => ({
-    blogs: [],
-    loading: false,
-    error: null,
-  }),
-  actions: {
-    async fetchBlogs() {
-      this.loading = true;
-      this.error = null;
-      try {
-        const response = await fetch('YOUR_API_ENDPOINT'); // Replace with your API endpoint
-        if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-        }
-        const data: Blog[] = await response.json();
-        this.blogs = data;
-      } catch (error) {
-        this.error =
-          error instanceof Error ? error.message : 'Failed to fetch blogs';
-      } finally {
-        this.loading = false;
-      }
-    },
-    async fetchBlogById(id: number): Promise<Blog | undefined> {
-      try {
-        const response = await fetch(`YOUR_API_ENDPOINT/${id}`); // Adjust API endpoint to fetch specific blog by id
-        if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-        }
-        const blogData: Blog = await response.json();
-        return blogData;
-      } catch (error) {
-        this.error =
-          error instanceof Error ? error.message : 'Failed to fetch blog data';
-        return undefined; // Return undefined in case of error
-      }
-    },
-  },
+  const blogs = ref<[Blog]|null>(null);
+  const loading = ref<boolean>(false);
+
+  function fetchBlogs(){
+    loading.value = true;
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+      .then((res) => {
+        blogs.value = res.data.slice(1, 21)
+        loading.value = false
+      })
+  }
+
+  return { blogs, fetchBlogs }
+
 });
